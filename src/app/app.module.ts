8e5ff27172fa385904db1c9a2a423d1a7a77f3b8
Anderson Misson <missondeveloper@gmail.com>
2020-12-15 17:48:52 -0300

    Adicionando tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { AppComponent } from './app.component';
     InputTextModule,
     ButtonModule,
     TableModule,
+    TooltipModule,
     AppRoutingModule
   ],
   providers: [],
