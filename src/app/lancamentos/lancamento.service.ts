5f566709430a0a74ba8c6ea7c0babd489869882e
Anderson Misson <missondeveloper@gmail.com>
2021-01-28 22:48:06 -0300

    Adicionando filtro por datas na pesquisa de lançamentos

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -1,8 +1,12 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import * as moment from 'moment';
+
 export interface LancamentoFiltro {
   descricao: string;
+  dataVencimentoInicio: Date;
+  dataVencimentoFim: Date;
 }
 
 @Injectable()
@@ -19,6 +23,16 @@ export class LancamentoService {
 
     if (filtro.descricao) {
       params = params.set('descricao', filtro.descricao);
+    }
+
+    if (filtro.dataVencimentoInicio) {
+      params = params.set('dataVencimentoDe',
+        moment(filtro.dataVencimentoInicio).format('DD-MM-YYYY'));
+    }
+
+    if (filtro.dataVencimentoFim) {
+      params = params.set('dataVencimentoAte',
+        moment(filtro.dataVencimentoFim).format('DD-MM-YYYY'));
     }
 
     return this.http.get(`${this.lancamentosUrl}?resumo`, { headers, params })
