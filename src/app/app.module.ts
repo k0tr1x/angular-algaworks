a3822afdf4e19d7d5e185b8316edf0bb40087ce1
Anderson Misson <missondeveloper@gmail.com>
2021-02-17 00:25:25 -0300

    Alterando o locale da aplicação para pt-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'
 
 import { MessageService, ConfirmationService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
@@ -17,6 +19,8 @@ import { LancamentosModule } from './lancamentos/lancamentos.module';
 
 import { PessoaService } from './pessoas/pessoa.service';
 import { LancamentoService } from './lancamentos/lancamento.service';
+
+registerLocaleData(localePt); // Para arrumar o modo dos numeros, ponto e virgula
 
 @NgModule({
   declarations: [
@@ -38,6 +42,7 @@ import { LancamentoService } from './lancamentos/lancamento.service';
     AppRoutingModule
   ],
   providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR'},
     MessageService,
     ConfirmationService,
     LancamentoService,
