ebd99f15f51c5f79c1e9d1b8f3d70e07ab9ec34f
Anderson Misson <missondeveloper@gmail.com>
2021-03-02 15:46:08 -0300

    Tratando casos de erros e sucesso de autenticação

--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AuthService } from './../auth.service';
+
+import { ErrorHandlerService } from './../../core/error-handler.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
 
-  constructor(private auth: AuthService) { }
+  constructor(
+    private auth: AuthService,
+    private errorHandler: ErrorHandlerService,
+    private router: Router
+  ) { }
 
   login(usuario: string, senha: string) {
-    this.auth.login(usuario, senha);
-  }
-
-  ngOnInit(): void {
+    this.auth.login(usuario, senha)
+      .then(() => {
+        this.router.navigate(['/lancamentos']);
+      })
+      .catch(erro => {
+        this.errorHandler.handle(erro);
+      });
   }
 
 }
