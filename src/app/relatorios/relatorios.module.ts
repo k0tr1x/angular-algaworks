0f0f5ba46cea8877a9f16a38b1eb07bbaea59031
Anderson Misson <missondeveloper@gmail.com>
2021-03-25 15:30:56 -0300

    Configurando formulário do relatório

--- src/app/relatorios/relatorios.module.ts
+++ src/app/relatorios/relatorios.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from './../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
+import { CalendarModule } from 'primeng/calendar';
+
+import { SharedModule } from './../shared/shared.module';
 import { RelatoriosRoutingModule } from './relatorios-routing.module';
 import { RelatorioLancamentosComponent } from './relatorio-lancamentos/relatorio-lancamentos.component';
 
 
 @NgModule({
-  declarations: [
-    RelatorioLancamentosComponent
-  ],
   imports: [
     CommonModule,
+    FormsModule,
+
+    CalendarModule,
 
     SharedModule,
     RelatoriosRoutingModule
+  ],
+  declarations: [
+    RelatorioLancamentosComponent
   ]
 })
 export class RelatoriosModule { }
